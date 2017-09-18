164a18849e55708e7d5c1d406f29c5acc7298bf2
mulleer <mulleeryry@gmail.com>
2017-09-18 14:45:33 -0400

    gopigo cha cha

---
+++ Main.py
@@ -0,0 +1,21 @@
+ import gopigo
+ import time
+
+ class Piggy(object):
+
+     def __init__(self):
+         print("I am alive")
+
+     def cha_cha(self):
+         for x in range(5):
+            right_rot()
+            time.sleep(.5)
+            left_rot()
+            time.sleep(.5)
+            stop(
+
+
+p = Piggy()
+p.cha_cha()
+
+
