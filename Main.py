0656e1b2d378610fa5ab05708aee24bdf31540e6
mulleer <mulleeryry@gmail.com>
2017-09-20 14:23:17 -0400

    gopigo cha cha

--- Main.py
+++ Main.py
@@ -25,7 +25,44 @@ class Piggy(object):
             time.sleep(.5)
             stop()
 
+
+
+    def pulse(self):
+        """check for obstacles, drive fixed amount forward"""
+        look = us_dist(15)  # store the distance reading
+        if look > 80:
+            fwd()
+            time.sleep(1)
+            stop()
+
+
+
+    def cruise(self):
+        """drive forward, stop if sensor detects obstacle"""
+        fwd()
+        while(True):
+            if us_dist(15) < 30:
+                stop()
+            time.sleep(.2)
+
+
+
+
+
+
+
+    def servo_sweep(self):
+        """loops in a 120 degree arc and moves servo"""
+        for ang in range(20, 160, 2):
+            servo(ang)
+            time.sleep(.2)
+
+
+
+
+
+
 p = Piggy()
-p.dab()
+
 
 
